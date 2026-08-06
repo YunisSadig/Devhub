359a376ca680c699f52992c375ee56c1ef4e73b9
YunisSadig <yunissadiq6@gmail.com>
2026-08-06 10:52:16 +0400

    feat: configure JWT environment properties

--- src/main/java/com/devhub/devhub/security/config/PasswordConfig.java
+++ src/main/java/com/devhub/devhub/security/config/PasswordConfig.java
@@ -1,4 +1,4 @@
-package com.devhub.devhub.security;
+package com.devhub.devhub.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
