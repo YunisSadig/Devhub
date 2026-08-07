2cd1d7720c5a219038aa75be7078fafe0b83cb00
YunisSadig <yunissadiq6@gmail.com>
2026-08-07 09:59:15 +0400

    feat: update Entity and migrations

--- src/main/java/com/devhub/devhub/user/User.java
+++ src/main/java/com/devhub/devhub/user/User.java
@@ -9,6 +9,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @Table(
@@ -25,8 +26,8 @@ import java.time.LocalDateTime;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
     @Version
     private Long version;
